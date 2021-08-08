affb36efad32712efab14ab7ebbfe8b34deff2be
Mark <mark-smirnow@yandex.ru>
2021-08-08 22:21:38 +0300

    temporary disable writing speed,power,battery session history

--- sessions.py
+++ sessions.py
@@ -45,10 +45,10 @@ class SessionInfo:
             self.average_battery_current = round(self.__av_battery_current_calc_sum / self.__av_battery_current_calc_count, 2)
             self.maximum_battery_current = round(max(self.maximum_battery_current, battery_current), 2)
 
-            self.speed_session_history.append(state.speed)
-            self.power_session_history.append(state.full_power)
-            self.battery_session_history.append(state.battery_percent)
-            self.ts_session_history.append(state.builded_ts_ms)
+            #self.speed_session_history.append(state.speed)
+            #self.power_session_history.append(state.full_power)
+            #self.battery_session_history.append(state.battery_percent)
+            #self.ts_session_history.append(state.builded_ts_ms)
 
         self.maximum_fet_temp = max(self.maximum_fet_temp,
                                     max(state.esc_a_state.temperature, state.esc_b_state.temperature))
